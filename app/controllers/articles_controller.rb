431d6cf5dbf00e19b547eb3f076513a3b3695859
Christopher Huynh & Joe Moorhouse <pair+chhuynh.jbhouse@devbootcamp.com>
2017-03-27 19:59:41 -0700

    added the ability to created articles and redirect to their show page

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,42 +1,47 @@
 class ArticlesController < ApplicationController
 
     def index
-      # @articles = Article.all
+      @articles = Article.all
     end
 
     def show
-      # @article = Article.find(params[:id])
+      @article = Article.find(params[:id])
     end
 
     def new
-      # @article = Article.new
+      # gets the form for create
+      @article = Article.new
+      # @article.category_id = params[:category_id]
     end
 
     def edit
-      # @article = Article.find(params[:id])
+      @article = Article.find(params[:id])
     end
 
     def create
-      # @article = Article.new(article_params)
-      #
-      # if @article.save
-      #   redirect_to @article
-      # else
-      #   render 'new'
-      # end
+      @article = Article.new(article_params)
+      @article.category_id = params[:category_id]
+      @category = Category.find(params[:category_id])
+      if @article.save
+        # redirect_to @category, @article
+        redirect_to category_article_path(@category, @article)
+      else
+        # p @article
+        # p "*" * 100
+        # p @article.errors.full_messages
+        render 'new'
+      end
     end
 
     def destroy
-      #   @article = Article.find(params[:id])
-      #   @article.destroy
-      #
-      #   redirect_to articles_path
-      # end
-      #
-      # private
-      #   def article_params
-      #     params.require(:article).permit(:title, :text)
-      #   end
+        @article = Article.find(params[:id])
+        @article.destroy
+
+        redirect_to articles_path
     end
 
+      private
+        def article_params
+          params.require(:article).permit(:title, :body)
+        end
 end
--- app/controllers/categories_controller.rb
+++ app/controllers/categories_controller.rb
@@ -6,37 +6,19 @@ class CategoriesController < ApplicationController
 
     def show
       @category = Category.find(params[:id])
+      @articles = Article.where(category_id: params[:id])
     end
 
     def new
-      # @category = Category.new
     end
 
     def edit
-      # @category = Category.find(params[:id])
     end
 
     def create
-      # @category = Category.new(category_params)
-      #
-      # if @category.save
-      #   redirect_to @category
-      # else
-      #   render 'new'
-      # end
     end
 
     def destroy
-      #   @category = Category.find(params[:id])
-      #   @category.destroy
-      #
-      #   redirect_to categorys_path
-      # end
-      #
-      # private
-      #   def category_params
-      #     params.require(:category).permit(:title, :text)
-      #   end
     end
 
 end
